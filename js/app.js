ba106c5a35216595cd301c3f47d2f93eab2e3d57
Sean Johnston <sean.johnst@gmail.com>
2015-06-19 09:13:27 -0400

    add type query

--- js/app.js
+++ js/app.js
@@ -1,16 +1,66 @@
 var myNumber = Math.ceil(Math.random() * 100);
 var guesses = 5;
+var firstDone = false;
+
+var synsAwful, synsOkay, synsCompetent, synsSmart, synsPerson, type;
 
 var insults = {
-  '100': [
-    'You could not be farther. ',
-    'You could not be farther. '],
-  '50': [
-    'Try again with more trying. ',
-    'Nice try, meat mind. '],
-  '20': [
-    'Not bad for a flesh plank. ',
-    'Maybe keep your day job. '],
+  '100': function() {
+    if (!synsAwful) {
+      synsAwful = getSynAdjs('stupid');
+    }
+    if (!synsPerson) {
+      synsPerson = getSynNouns(type);
+    }
+    var adj = randomItem(synsAwful);
+    var aOrAn = getAorAn(adj);
+    return 'You are ' + aOrAn + adj + ' ' + randomItem(synsPerson) + '.';
+  },
+  '50': function() {
+    if (!synsOkay) {
+      synsOkay = getSynAdjs('average');
+    }
+    if (!synsPerson) {
+      synsPerson = getSynNouns(type);
+    }
+    var adj = randomItem(synsOkay);
+    var aOrAn = getAorAn(adj);
+    return 'You are ' + aOrAn + adj + ' ' + randomItem(synsPerson) + '.';
+  },
+  '20': function() {
+    if (!synsCompetent) {
+      synsCompetent = getSynAdjs('competent');
+    }
+    if (!synsPerson) {
+      synsPerson = getSynNouns(type);
+    }
+    var adj = randomItem(synsCompetent);
+    var aOrAn = getAorAn(adj);
+    return 'You are ' + aOrAn + adj + ' ' + randomItem(synsPerson) + '.';
+  },
+  '5': function() {
+    if (!synsSmart) {
+      synsSmart = getSynAdjs('intelligent');
+    }
+    if (!synsPerson) {
+      synsPerson = getSynNouns(type);
+    }
+    var adj = randomItem(synsSmart);
+    var aOrAn = getAorAn(adj);
+    return 'You are ' + aOrAn + adj + ' ' + randomItem(synsPerson) + '.';
+  },
+}
+
+function getAorAn(string) {
+  if (string[0].match(/[aeiou]/)) {
+    return "an ";
+  } else {
+    return "a ";
+  }
+}
+
+function randomItem(array) {
+  return array[Math.floor(Math.random() * array.length)];
 }
 
 function httpGet(theUrl) {
@@ -43,7 +93,7 @@ function win() {
 
 function submit() {
   function insult(degree) {
-    var ins = insults[degree][Math.floor(Math.random() * 2)];
+    var ins = insults[degree]();
     if (diff < 0) {
       ins += " Try higher.";
     }
@@ -52,9 +102,11 @@ function submit() {
     }
     $('.formsep').after('<p class="response">' + ins + '</p>')
   }
+
   $('.response').remove();
   $('.answer').remove();
   guesses--;
+
   var guess = $('#guess').val();
   var diff = guess - myNumber;
 
@@ -68,6 +120,8 @@ function submit() {
     $('.submit-button').addClass('disabled');
     $('.response').remove();
     $('.formsep').after('<p class="response">Too bad. You lose.</p>')
+  } else if (Math.abs(diff) < 20) {
+    insult(5);
   } else if (Math.abs(diff) < 20) {
     insult(20);
   } else if (Math.abs(diff) < 50) {
@@ -83,9 +137,21 @@ function submit() {
 }
 
 $(document).ready(function() {
-  $('#guess').focus()
+  if (!firstDone) {
+    $('#person').focus();
+  }
+  else {
+    $('#guess').focus();
+  }
 
-  $('.submit-button').on('click', submit)
+  $('.first-button').on('click', function() {
+    type = $('#person').val();
+    firstDone = true;
+    $('.query').addClass('hidden');
+    $('.main').removeClass('hidden')
+  });
+
+  $('.submit-button').on('click', submit);
 
   $("#guess").keyup(function (e) {
       if (e.keyCode == 13) {
