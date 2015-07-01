7980ac6d535d1d7d7fe56eb2d140352c87170033
Sean Johnston <sean.johnst@gmail.com>
2015-07-01 18:42:54 -0500

    add ajax req, add more requirements

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 var myNumber = Math.ceil(Math.random() * 100);
 var guesses = 5;
-var firstDone = false;
 var guessList = [];
+var hotList = [];
+var coldList = [];
 
-var synsAwful, synsOkay, synsCompetent, synsSmart, synsPerson, type;
+var type;
 
 var queries = {
   person: ["individual","someone","somebody","mortal","soul","anatomy",
@@ -14,12 +15,12 @@ var queries = {
   human: ["person", "man","human being","hominid","anthropoid"],
   stupid: ["dazed","stunned","stupefied","stupid","unintelligent",
   "retarded","anserine","blockheaded","boneheaded","brainless","cloddish","confused",
-  "dense","dim","doltish","dopey","dopy","dull","dumb","fatheaded","feebleminded",
+  "dense","dim","doltish","dopey","dull","dumb","fatheaded","feebleminded",
   "foolish","gaumless","gooselike","goosey","goosy","gormless","half-witted",
-  "headless","idiotic","imbecile","imbecilic","jerky","loggerheaded","lumpen",
+  "headless","idiotic","imbecilic","jerky","loggerheaded","lumpen",
   "lumpish","moronic","nitwitted","obtuse","senseless","slow","slow-witted",
   "soft-witted","thick","thick-skulled","thickheaded","unthinking","weak","witless"],
-  average: ["ordinary","fair","mediocre","middling","intermediate","medium","mean",
+  average: ["ordinary","fair","mediocre","middling","intermediate",
   "median","modal","ordinary","common","moderate","normal"],
   competent: ["capable","efficient","qualified","able","adequate",
   "effective"],
@@ -48,7 +49,12 @@ function phrase(word, phraseType) {
 
 function standardPhrase(adj, noun) {
   var aOrAn = getAorAn(adj);
-  return 'You are ' + aOrAn + adj + ' ' + randomItem(synsPerson) + '.';
+  return 'You are ' + aOrAn + adj + ' ' + randomItem(synsPerson) + '. ';
+}
+
+function iceColdPhrase(adj, noun) {
+  var prePhrase = standardPhrase(adj, noun);
+  return prePhrase;
 }
 
 
@@ -89,7 +95,7 @@ function httpGet(theUrl) {
 function getSynonyms(word) {
   var url = 'http://words.bighugelabs.com/api/2/777c02c907809fb4a79cfb3f62b1ab3c/' + 
     word +'/json';
-  var jsonObj = JSON.parse(httpGet(url));
+  var jsonObj = httpGet(url);
   return jsonObj;
 }
 
@@ -112,24 +118,30 @@ function submit() {
   function insult(degree) {
     var ins = insults[degree]();
     if (diff < 0) {
-      ins += " Try higher.";
+      ins += '<p>Try higher.</p>';
     }
     else {
-      ins += " Try lower."
+      ins += '<p>Try lower.</p>';
     }
-    $('.formsep').after('<p class="response">Not ' + guess + '. ' + ins + '</p>')
+    $('.response').append('<p>Not ' + guess + '. ' + ins + '</p>');
   }
 
-  $('.response').remove();
-  $('.answer').remove();
+  $('.response').html('');
 
   var guess = $('#guess').val();
 
   if (!guess) {
-    $('.formsep').after('<p class="response">You didn\'t guess anything.</p>')
+    $('.response').append('<p>You didn\'t guess anything.</p>')
     return;
   } else if (guessList.indexOf(guess) != -1) {
-    $('.formsep').after('<p class="response">You\'ve already guessed ' + guess + '.</p>')
+    $('.response').append('<p>Idiot! You\'ve already guessed ' + guess + '.</p>')
+    return;
+  } else if (isNaN(guess)) {
+    guess = guess[0].toUpperCase() + guess.split('').slice(1).join('');
+    $('.response').append('<p>' + guess + ' isn\'t even a number.</p>');
+    return;
+  } else if (guess < 1 || guess > 100) {
+    $('.response').append('<p>Right. I\'m gonna pretend you didn\'t guess out of the range.</p>')
     return;
   }
 
@@ -141,38 +153,67 @@ function submit() {
   $('#guess').val("");
 
   if (guess == myNumber) {
-    $('.formsep').after('<p class="response">You got it! ' + guess + '!</p>');
+    $('.response').append('<p>You got it! ' + guess + '!</p>');
     win();
     $('.submit-button').addClass('disabled');
+    $("#guess").off('.guess');
   } else if (guesses < 1) {
     $('.submit-button').addClass('disabled');
-    $('.response').remove();
-    $('.formsep').after('<p class="response">Too bad. You lose. Correct number was ' + myNumber + '</p>')
-  } else if (Math.abs(diff) < 20) {
+    $('.response').html('');
+    $('.response').append('<p class="response">Too bad. You lose. Correct number was ' + myNumber + '.</p>')
+    $("#guess").off('.guess');
+  } else if (Math.abs(diff) < 5) {
     insult(5);
+    showGuess('hot', guess);
   } else if (Math.abs(diff) < 20) {
     insult(20);
-  } else if (Math.abs(diff) < 50) {
+    showGuess('hot', guess);
+    } else if (Math.abs(diff) < 50) {
     insult(50);
+    showGuess('cold', guess);
   } else {
     insult(100);
+    showGuess('cold', guess);
   }
 
-  guessList.push()
+  guessList.push(guess);
 
   $('.guess-display').text(guesses);
-  $('#guess').focus();5
 
-  //$('.formsep').after('<p class="answer">My number was ' + myNumber + '</p>');
-  //$('.formsep').after('<p class="answer">Your guess was ' + guess + '</p>');
+  $('#guess').val('').focus();
+
+  //$('.response').append('<p class="answer">My number was ' + myNumber + '</p>');
+  //$('.response').append('<p class="answer">Your guess was ' + guess + '</p>');
+}
+
+function showGuess(str, guess) {
+  $('<li></li>').append(guess).addClass(str + '-guess').appendTo($('.' + str+ '-guesses'));
+  $('.' + str + '-guesses').removeClass('hidden');
 }
 
 function firstTask() {
-  if ($('#person').val())
+  if ($('#person').val()) {
     type = $('#person').val();
-  else
+    var url = 'http://words.bighugelabs.com/api/2/777c02c907809fb4a79cfb3f62b1ab3c/' + 
+      type +'/json';
+    $.ajax(url, {
+      type: 'GET',
+      dataType: 'json',
+      success: function(response) {
+        queries[type] = response.noun.syn;
+      },
+      error: function() {
+        $('.formsep').append('<p class="response">Sorry, I don\'t know you. I\'m assuming you\'re a person.</p>')
+        type = 'person';
+      }
+    });
+  } else
     type = "person";
-  firstDone = true;
+
+
+
+
+
   $('.query').addClass('hidden');
   $('.main').removeClass('hidden');
   $('#guess').focus();
@@ -194,9 +235,9 @@ $(document).ready(function() {
     }
   });
 
-  $('.submit-button').on('click', submit);
+  $('.submit-button').on('click.guess', submit);
 
-  $("#guess").keyup(function (e) {
+  $("#guess").on('keyup.guess', function (e) {
     if (e.keyCode == 13) {
       submit();
     }
